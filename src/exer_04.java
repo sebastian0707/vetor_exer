eadd46028ac1d2f259bc73f5b219fcef11a5d274
sebastiana0707 <floressebastian725@gmail.com>
2023-10-30 17:24:28 -0300

    Adição de um exercício

---
+++ src/exer_04.java
@@ -0,0 +1,10 @@
+//Elaborar um programa que leia 6 números e exiba:
+//
+//o maior número lido;
+//o menor número lido;
+//quantos números são iguais ao primeiro número.
+public class exer_04 {
+    public static void main(String[] args) {
+
+    }
+}
